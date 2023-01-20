d0e211cc715354f3eb3e508b7c5b3263dee3a0b7
Cristobal Cepeda <cceplag@gmail.com>
2023-01-20 12:02:00 +0900

    refactor using just the payout id instead of time

--- cloud_stripe_report/webhook/payout_wh.py
+++ cloud_stripe_report/webhook/payout_wh.py
@@ -1,33 +1,31 @@
 from google.cloud import bigquery
 import stripe
 import os
-from datetime import datetime, date, time,timedelta
 
 
-def report_request(ending_time):
+def report_request(payout_id):
 
     """
-    Function generates a request for a report that covers from friday one week ago to saturday 2 days ago
-    assuming the function is triggered every monday
+    Function generates a request for a report that covers the last payout that has been deposited
     """
 
     api_key_local = os.environ.get('stripe_key')
-    table_q = os.environ.get('TABLE_QUERY')
+    # table_q = os.environ.get('TABLE_QUERY')
     stripe.api_key = api_key_local
 
     ## Query to get last transaction in bq
-    query =f"""SELECT max(automatic_payout_effective_at) FROM `{table_q}`""" ## ORIGINAL
-    client_q = bigquery.Client()
-    query_job = client_q.query(query)  # Make an API request.
-    rows = query_job.result()
-    result = list(rows)[0]
-    last_result_timestamp = int(datetime.timestamp(result[0])) + 1
+    # query =f"""SELECT max(automatic_payout_effective_at) FROM `{table_q}`""" ## ORIGINAL
+    # client_q = bigquery.Client()
+    # query_job = client_q.query(query)  # Make an API request.
+    # rows = query_job.result()
+    # result = list(rows)[0]
+    # last_result_timestamp = int(datetime.timestamp(result[0])) + 1
     ## Start period from last payment in table
-    start_var = last_result_timestamp
+    # start_var = last_result_timestamp
 
     ## end_time is at 23:59:59 hrs 1 day before the execution of the funciton
 
-    end_var = str(ending_time)
+    # end_var = str(ending_time)
 
     ## Type of report to create
     ## ref: https://stripe.com/docs/reports/report-types
@@ -36,8 +34,7 @@ def report_request(ending_time):
     report_dict = stripe.reporting.ReportRun.create(
     report_type = report_var,
     parameters={
-      'interval_start': start_var,
-      'interval_end': end_var,
+      'payout': payout_id,
       'columns': [
         'automatic_payout_id',
         'automatic_payout_effective_at',
