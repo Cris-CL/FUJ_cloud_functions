ea7ef517afc98b8934d93a6e3bfe0df6de3d5b0c
Cristobal Cepeda <cceplag@gmail.com>
2024-02-06 12:39:20 +0900

    ADD template for docstrings

---
+++ docstring_template.py
@@ -0,0 +1,29 @@
+def function_name(parameter1, parameter2):
+    """
+        Brief description of the function.
+
+        A more detailed description of the function, including information about
+        its parameters, return values, and any exceptions it may raise.
+
+        Parameters:
+        - parameter1 (type): Description of parameter1.
+        - parameter2 (type): Description of parameter2.
+        - ... (type): ...
+
+        Returns:
+        - return_type: Description of the return value.
+
+        Raises:
+        - ExceptionType: Description of when this exception might be raised.
+
+        Example:
+        ```python
+        # Example usage of the function
+        result = function_name(arg1, arg2)
+        print(result)
+        ```
+
+        Additional notes or examples if necessary.
+
+        """
+    pass
