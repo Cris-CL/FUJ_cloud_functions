345e3e821673013180f4ad1cf55d274a44083ef6
Cristobal Cepeda <cceplag@gmail.com>
2024-11-26 13:36:55 +0900

    shopify_daily_orders latest deployed version

--- shopify_api/get_orders.py
+++ shopify_api/get_orders.py
@@ -10,15 +10,7 @@ import pandas as pd
 from google.cloud import bigquery
 from google.cloud import storage
 from datetime import datetime, date
-
-
-apikey = os.environ.get("SHOPIFY_KEY")
-api_version = os.environ.get("API_VERSION")
-password = os.environ.get("SHOPIFY_PASS")
-bucket_name = os.environ.get("BUCKET")
-project_name = os.environ.get("PROJECT_NAME")
-table_name = os.environ.get("TABLE_NAME")
-shop_name = os.environ.get("STORE_NAME")
+from orders_parameters import *
 
 
 def get_all_orders(last_order_id):
@@ -41,63 +33,17 @@ def get_all_orders(last_order_id):
     limit = 250
     status = "any"
     orders = pd.DataFrame()
-    fields = ",".join(
-        [
-            "id",
-            "name",
-            "email",
-            "created-at",
-            "cancelled-at",
-            "confirmed",
-            "cancel-reason",
-            "buyer-accepts-marketing",
-            "contact-email",
-            "current-subtotal-price",
-            "current-total-price",
-            "total-outstanding",
-            "current-total-tax",
-            "current-total-discounts",
-            "discount-codes",
-            "financial-status",
-            "fulfillment-status",
-            "refunds",  ### added 18/01/23 for get the refund amounts
-            "note",
-            "number",
-            "order-number",
-            "payment-gateway-names",
-            "processed-at",
-            "reference",
-            "source-identifier",
-            "source-name",
-            "line-items",
-            "updated-at",
-            # "gateway",
-            # "processing-method", #### deprecated
-            # "cart-token", #### deprecated
-            # "checkout-token", #### deprecated
-            # "token", #### deprecated
-            "note-attributes",
-            "checkout-id",
-            "tags",
-            "refering-site",
-            "totat-line-items-price",
-            "total_shipping_price_set",
-            "subtotal-price",
-            "total-price",
-            "total-tax",
-            "shipping-address",
-            "total-discounts",  ### Added total discounts field
-        ]
-    )
+    fields = ",".join(fields_api)
+
     print(f"First order_id: {last}")
     while True:
-        url = f"https://{apikey}:{password}@{shop_name}.myshopify.com/admin/api/{api_version}/orders.json?limit={limit}&fields={fields}&status={status}&since_id={last}"
+        url = f"https://{SHOPIFY_KEY}:{SHOPIFY_PASS}@{STORE_NAME}.myshopify.com/admin/api/{API_VERSION}/orders.json?limit={limit}&fields={fields}&status={status}&since_id={last}"
         response_in = requests.get(url)
         try:
             df = pd.json_normalize(response_in.json()["orders"])
         except Exception as e:
             print("get_all_orders failed with error:")
-            print(e,type(e))
+            print(e, type(e))
             return pd.DataFrame()
 
         # Function finishes with no new info
@@ -232,7 +178,6 @@ def discount_process(df):
     df["payment_gateway_names"] = df["payment_gateway_names"].apply(
         lambda x: clean_row(x) if len(x) > 0 else None
     )
-
     df.drop(columns="discount_codes", inplace=True)
     return df
 
@@ -255,8 +200,12 @@ def refund_clean(val):
         elif isinstance(x, dict):
             if len(x.get("refund_line_items", 0)) < 1:
                 continue
-            # tmp = tmp + float(x.get("refund_line_items", 0)[0].get("subtotal", 0))
-            tmp += sum([float(part.get("subtotal", 0)) for part in x.get("refund_line_items", {})])
+            tmp += sum(
+                [
+                    float(part.get("subtotal", 0))
+                    for part in x.get("refund_line_items", {})
+                ]
+            )
     return tmp
 
 
@@ -270,7 +219,6 @@ def refunds(df):
     Returns:
     - df (pd.DataFrame): The same dataframe, but with a new column extracted from the refunds column
     """
-
 
     df["refund"] = df["refunds"].map(lambda x: refund_clean(x) if len(x) > 0 else 0)
     df.drop(columns="refunds", inplace=True)
@@ -295,7 +243,7 @@ def get_transactions(id_list):
     transactions = pd.DataFrame()
     for order_id in set(id_list):
 
-        url = f"https://{apikey}:{password}@{shop_name}.myshopify.com/admin/api/{api_version}/orders/{order_id}/transactions.json"
+        url = f"https://{SHOPIFY_KEY}:{SHOPIFY_PASS}@{STORE_NAME}.myshopify.com/admin/api/{API_VERSION}/orders/{order_id}/transactions.json"
         response_in = requests.get(url)
         df = pd.json_normalize(response_in.json()["transactions"][0])
         for row in df.iterrows():
@@ -348,6 +296,7 @@ def join_orders_transactions(orders_df, transactions_df):
 
     return new_df
 
+
 def stripe_process(df):
     """
     Process the stripe orders, and returns the processed dataframe
@@ -373,7 +322,7 @@ def stripe_process(df):
 
 
 def type_change(df):
-    """"
+    """ "
     Changes the type of the columns in the dataframe, and returns the processed dataframe
 
     This function changes the type of the columns in the dataframe to the
@@ -385,78 +334,6 @@ def type_change(df):
     - df (pd.DataFrame): The same dataframe, but with the columns changed to the correct type
 
     """
-    deprecated = [
-        "gateway",
-        "cart_token",  #### deprecated
-        "checkout_token",  #### deprecated
-        "token",  #### deprecated
-        "processing_method",
-    ]
-    dict_types = {
-        "id": "str",
-        "buyer_accepts_marketing": "bool",
-        "cancel_reason": "str",
-        "cancelled_at": "datetime64[ns]",
-        "cart_token": "str",
-        "checkout_id": "str",
-        "checkout_token": "str",
-        "confirmed": "str",
-        "contact_email": "str",
-        "created_at": "datetime64[ns]",
-        "current_subtotal_price": "float64",
-        "current_total_discounts": "float64",
-        "current_total_price": "float64",
-        "current_total_tax": "str",
-        "email": "str",
-        "financial_status": "str",
-        "fulfillment_status": "str",
-        "refund": "float64",
-        "name": "str",
-        "note": "str",
-        "note_attributes": "str",
-        "number": "str",
-        "order_number": "str",
-        "payment_gateway_names": "str",
-        "processed_at": "datetime64[ns]",
-        "processing_method": "str",
-        "reference": "str",
-        "source_identifier": "str",
-        "source_name": "str",
-        "tags": "str",
-        "token": "str",
-        "total_outstanding": "float64",
-        "updated_at": "datetime64[ns]",
-        "shipping_shop_amount": "float64",
-        "shipping_presentment_amount": "float64",
-        "lineitem_quantity": "float64",
-        "lineitem_name": "str",
-        "lineitem_price": "float64",
-        "lineitem_sku": "str",
-        "discount_code": "str",
-        "discount_amount": "float64",
-        "discount_type": "str",
-        "subtotal_price": "float64",
-        "total_price": "float64",
-        "total_tax": "float64",
-        "UPDATED_FROM_API": "datetime64[ns]",
-        "ship_address1": "str",
-        "ship_phone": "str",
-        "ship_city": "str",
-        "ship_zip": "str",
-        "ship_province": "str",
-        "ship_country": "str",
-        "ship_first_name": "str",
-        "ship_last_name": "str",
-        "ship_address2": "str",
-        "ship_company": "str",
-        "ship_name": "str",
-        "ship_latitude": "float64",
-        "ship_longitude": "float64",
-        "ship_country_code": "str",
-        "ship_province_code": "str",
-        "total_discounts": "float64",
-        "lineitem_discount": "float64",
-    }
 
     for cols_db in dict_types.keys():
         if cols_db not in df.columns:
@@ -469,7 +346,10 @@ def type_change(df):
     ## Delete nan strings or empty values
     for col in df.columns:
         df[col] = df[col].apply(
-            lambda x: None if x in ["nan", "", "None", "null"] else x
+            lambda x: None
+            if isinstance(x, str)
+            and x.lower() in ["nan", "none", "null", "na", "<na>", "<nan>", ""]
+            else x
         )
         if col not in dict_types.keys():
             ##### dropping columns that shouldnt appear
@@ -483,10 +363,8 @@ def type_change(df):
     return df.copy()
 
 
-
-
 def upload_to_bq(df, today_date, result):
-    """"
+    """ "
     Uploads the dataframe to BigQuery
 
     This function uploads the dataframe to BigQuery, and if it fails,
@@ -503,8 +381,8 @@ def upload_to_bq(df, today_date, result):
 
     try:
         df.to_gbq(
-            destination_table=table_name,
-            project_id=project_name,
+            destination_table=TABLE_NAME,
+            project_id=PROJECT_NAME,
             progress_bar=False,
             if_exists="append",  ### should be append
         )
@@ -514,14 +392,15 @@ def upload_to_bq(df, today_date, result):
         print(e, type(e))
         print("Saving data to bucket")
         storage_client = storage.Client()
-        bucket = storage_client.list_buckets().client.bucket(bucket_name)
+        bucket = storage_client.list_buckets().client.bucket(BUCKET)
         file_name = f"SH_problem_data_{today_date}_{result}_RAW.csv"
         blob = bucket.blob(file_name)
         blob.upload_from_string(df.to_csv(index=False), content_type="csv/txt")
     return
 
+
 def delete_pending_orders():
-    """"
+    """ "
     Deletes the pending orders from the BigQuery table
 
     This function deletes the pending orders from the BigQuery table every monday
@@ -540,11 +419,11 @@ def delete_pending_orders():
         ## Changed to better select the last order in the previous 2 weeks
         q_tmp = f"""
         -- deleting last 2 weeks get_orders function
-        DELETE `{project_name}.{table_name}`
+        DELETE `{PROJECT_NAME}.{TABLE_NAME}`
         Where
             CAST(order_number as int64) > (
                 SELECT max(CAST(order_number as int64))
-                FROM `{project_name}.{table_name}`
+                FROM `{PROJECT_NAME}.{TABLE_NAME}`
                 WHERE created_at < CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 14 DAY) AS TIMESTAMP)
                 -- today minus 14 days
             )
@@ -558,6 +437,7 @@ def delete_pending_orders():
             print(f"Unexpected {err=}, {type(err)=}")
             print("Couldn'd delete the last 2 weeks orders")
     return
+
 
 def get_last_order_id():
     """
@@ -577,8 +457,8 @@ def get_last_order_id():
     ## query select the id correspondig to the last order in the table
     query = f"""
     select distinct id
-    FROM `{project_name}.{table_name}`
-    Where CAST(order_number as integer) = (SELECT max(CAST(order_number as integer)) FROM `{project_name}.{table_name}`)
+    FROM `{PROJECT_NAME}.{TABLE_NAME}`
+    Where CAST(order_number as integer) = (SELECT max(CAST(order_number as integer)) FROM `{PROJECT_NAME}.{TABLE_NAME}`)
     """
     ##### previously the max(name) caused problems because it was an string and the max string was #9999 and since then the orders were duplicated
 
@@ -594,6 +474,7 @@ def get_last_order_id():
         result = 9999999999999
     return result
 
+
 def main(data, context):
     """
     whole process from check the last order, to make api calls until the data is updated, and save that data as a
@@ -605,7 +486,9 @@ def main(data, context):
 
     df = get_all_orders(result)  ## 2270011949127 --> Reference id that works
 
-    if not isinstance(df, pd.DataFrame):  ### if the df is not a dataframe, it means that there is no new data
+    if not isinstance(
+        df, pd.DataFrame
+    ):  ### if the df is not a dataframe, it means that there is no new data
         return print("No new data to add")
     elif len(df) < 1:
         return print("No new data to add")
@@ -632,7 +515,7 @@ def main(data, context):
     if dt.weekday() == 0:
         print("Saving to bucket last 2 weeks of orders")
         storage_client = storage.Client()
-        bucket = storage_client.list_buckets().client.bucket(bucket_name)
+        bucket = storage_client.list_buckets().client.bucket(BUCKET)
         blob = bucket.blob(file_name)
         blob.upload_from_string(df.to_csv(index=False), content_type="csv/txt")
     return
---
+++ shopify_api/orders_parameters.py
@@ -0,0 +1,130 @@
+import os
+
+SHOPIFY_KEY = os.environ.get("SHOPIFY_KEY")
+API_VERSION = os.environ.get("API_VERSION")
+SHOPIFY_PASS = os.environ.get("SHOPIFY_PASS")
+BUCKET = os.environ.get("BUCKET")
+PROJECT_NAME = os.environ.get("PROJECT_NAME")
+TABLE_NAME = os.environ.get("TABLE_NAME")
+STORE_NAME = os.environ.get("STORE_NAME")
+
+fields_api = [
+    "id",
+    "name",
+    "email",
+    "created-at",
+    "cancelled-at",
+    "confirmed",
+    "cancel-reason",
+    "buyer-accepts-marketing",
+    "contact-email",
+    "current-subtotal-price",
+    "current-total-price",
+    "total-outstanding",
+    "current-total-tax",
+    "current-total-discounts",
+    "discount-codes",
+    "financial-status",
+    "fulfillment-status",
+    "refunds",  ### added 18/01/23 for get the refund amounts
+    "note",
+    "number",
+    "order-number",
+    "payment-gateway-names",
+    "processed-at",
+    "reference",
+    "source-identifier",
+    "source-name",
+    "line-items",
+    "updated-at",
+    # "gateway",
+    # "processing-method", #### deprecated
+    # "cart-token", #### deprecated
+    # "checkout-token", #### deprecated
+    # "token", #### deprecated
+    "note-attributes",
+    "checkout-id",
+    "tags",
+    "refering-site",
+    "totat-line-items-price",
+    "total_shipping_price_set",
+    "subtotal-price",
+    "total-price",
+    "total-tax",
+    "shipping-address",
+    "total-discounts",  ### Added total discounts field
+]
+
+deprecated = [
+    "gateway",
+    "cart_token",  #### deprecated
+    "checkout_token",  #### deprecated
+    "token",  #### deprecated
+    "processing_method",
+]
+
+dict_types = {
+    "id": "str",
+    "buyer_accepts_marketing": "bool",
+    "cancel_reason": "str",
+    "cancelled_at": "datetime64[ns]",
+    "cart_token": "str",
+    "checkout_id": "str",
+    "checkout_token": "str",
+    "confirmed": "str",
+    "contact_email": "str",
+    "created_at": "datetime64[ns]",
+    "current_subtotal_price": "float64",
+    "current_total_discounts": "float64",
+    "current_total_price": "float64",
+    "current_total_tax": "str",
+    "email": "str",
+    "financial_status": "str",
+    "fulfillment_status": "str",
+    "refund": "float64",
+    "name": "str",
+    "note": "str",
+    "note_attributes": "str",
+    "number": "str",
+    "order_number": "str",
+    "payment_gateway_names": "str",
+    "processed_at": "datetime64[ns]",
+    "processing_method": "str",
+    "reference": "str",
+    "source_identifier": "str",
+    "source_name": "str",
+    "tags": "str",
+    "token": "str",
+    "total_outstanding": "float64",
+    "updated_at": "datetime64[ns]",
+    "shipping_shop_amount": "float64",
+    "shipping_presentment_amount": "float64",
+    "lineitem_quantity": "float64",
+    "lineitem_name": "str",
+    "lineitem_price": "float64",
+    "lineitem_sku": "str",
+    "discount_code": "str",
+    "discount_amount": "float64",
+    "discount_type": "str",
+    "subtotal_price": "float64",
+    "total_price": "float64",
+    "total_tax": "float64",
+    "UPDATED_FROM_API": "datetime64[ns]",
+    "ship_address1": "str",
+    "ship_phone": "str",
+    "ship_city": "str",
+    "ship_zip": "str",
+    "ship_province": "str",
+    "ship_country": "str",
+    "ship_first_name": "str",
+    "ship_last_name": "str",
+    "ship_address2": "str",
+    "ship_company": "str",
+    "ship_name": "str",
+    "ship_latitude": "float64",
+    "ship_longitude": "float64",
+    "ship_country_code": "str",
+    "ship_province_code": "str",
+    "total_discounts": "float64",
+    "lineitem_discount": "float64",
+}
